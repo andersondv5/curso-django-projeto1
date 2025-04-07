9a08a558fdcc233e77cfe86dccf44606ab3bf84a
Anderson Silva <anderson.silvadev5@gmail.com>
2025-04-07 10:55:07 -0300

    Criado o app Django [recipes]

--- projeto/urls.py
+++ projeto/urls.py
@@ -19,10 +19,18 @@ from django.urls import path
 from django.http import HttpResponse
 
 # HTTP REQUEST
-def my_view(request):
-  return HttpResponse('STRING')
+def home(request):
+  return HttpResponse('HOME')
+
+def contato(request):
+  return HttpResponse('contato')
+
+def sobre(request):
+  return HttpResponse('sobre')
     
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sobre/', my_view), 
+    path('', home),
+    path('sobre/', sobre), # /sobre/
+    path('contato/', contato), # /contato/ 
 ]
