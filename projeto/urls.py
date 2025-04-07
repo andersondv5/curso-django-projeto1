0ec60fdc03ae36fc30ab6961b72bd40f16991952
Anderson Silva <anderson.silvadev5@gmail.com>
2025-04-07 11:31:02 -0300

    Refatoração de apps, urls e views

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,22 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-# HTTP REQUEST
-def home(request):
-  return HttpResponse('HOME')
-
-def contato(request):
-  return HttpResponse('contato')
-
-def sobre(request):
-  return HttpResponse('sobre')
+from django.urls import path, include
     
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('sobre/', sobre), # /sobre/
-    path('contato/', contato), # /contato/ 
+    path('', include('recipes.urls')),
 ]
