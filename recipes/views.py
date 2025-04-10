ef8b6ce846ffdbf361005d731159d177a362749a
Anderson Silva <anderson.silvadev5@gmail.com>
2025-04-10 10:06:25 -0300

    atualização de views e HTML

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-  return HttpResponse('HOME 1')
+  return render(request, 'recipes/home.html', context={
+    'name': 'Luiz Otávio',
+  })
 
 def contato(request):
   return HttpResponse('contato')
