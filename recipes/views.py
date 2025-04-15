42fb2a4c0786e29ea363e455c1bc59996f12b5c0
Anderson Silva <anderson.silvadev5@gmail.com>
2025-04-15 10:53:39 -0300

    Exemplo de template temp

--- recipes/views.py
+++ recipes/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 
 def home(request):
   return render(request, 'recipes/home.html', context={
-    'name': 'Luiz Otávio',
+    'name' : 'Luiz Otávio',
   })
 
 def contato(request):
-  return HttpResponse('contato')
+  return render(request, 'me-apague/temp.html')
 
 def sobre(request):
   return HttpResponse('sobre')
